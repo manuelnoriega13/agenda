5ef9a22499568b5bc90c0d5e3c0ac4f63e8446f0
manuel <manuelnoriega13@gmail.com>
2019-04-22 01:20:30 -0300

    security

--- src/main/java/com/manoriega/agenda/config/SpringSecurityConfig.java
+++ src/main/java/com/manoriega/agenda/config/SpringSecurityConfig.java
@@ -1,8 +1,39 @@
 package com.manoriega.agenda.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder())
+                .withUser("1234")
+                .password(passwordEncoder().encode("1234"))
+                .roles("USER");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                //HTTP Basic authentication
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/persona/list").hasRole("USER")
+                .and()
+                .csrf().disable()
+                .formLogin().disable();
+    }
 }
