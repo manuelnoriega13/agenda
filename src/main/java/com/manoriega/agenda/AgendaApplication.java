587041966c3b0c406d5e65fc4070946a8f3c852d
manuel <manuelnoriega13@gmail.com>
2019-05-24 00:05:07 -0300

    2

--- src/main/java/com/manoriega/agenda/AgendaApplication.java
+++ src/main/java/com/manoriega/agenda/AgendaApplication.java
@@ -1,18 +1,35 @@
 package com.manoriega.agenda;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
+@EnableEncryptableProperties
 @SpringBootApplication
 public class AgendaApplication {
 
 
-	public static void main(String[] args) {
-		SpringApplication.run(AgendaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AgendaApplication.class, args);
 
+    }
+
+//    @Value("${j}")
+    private String pro;
+
+    @Component
+    public class myRunner implements CommandLineRunner{
+        @Override
+        public void run(String... args) throws Exception {
+            System.out.println(pro);
+
+        }
+    }
 
 
 }
--- src/main/java/com/manoriega/agenda/config/security/SpringSecurityConfig.java
+++ src/main/java/com/manoriega/agenda/config/security/SpringSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.manoriega.agenda.config;
+package com.manoriega.agenda.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
